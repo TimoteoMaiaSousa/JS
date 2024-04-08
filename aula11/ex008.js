4d0907d957d772275a0aca9fb849f7baab02bd39
TimoteoMaiaSousa <156314160+TimoteoMaiaSousa@users.noreply.github.com>
2024-04-08 20:46:40 -0300

    Condições em JS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 79
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){console.log(`A velocidade máxima permitida é 60KM/h. Reduza sua velocidade em ${vel - 60}Km/h`)
+}else{
+    console.log(' Muito bem! Dirija sempre com cuidado e use cinto de segurança')
+}
