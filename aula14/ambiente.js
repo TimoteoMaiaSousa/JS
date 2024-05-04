d59a47851b8220d66c33a2ea16d4ac277254d591
TimoteoMaiaSousa <156314160+TimoteoMaiaSousa@users.noreply.github.com>
2024-05-04 13:55:04 -0300

    tarefas da escola

---
+++ aula14/ambiente.js
@@ -0,0 +1,19 @@
+/* Aprendendo a usar 'for' no JS */
+/* =-=-=-=-=-=-RELEMBRANDO-=-=-=-=-=-= */
+/* Repetição usando while:
+var c = 1
+while(c<=5){
+    console.log(c)
+    c+=1
+} */
+/* =-=-=-=-=-=-=-==-=-=-=-=-==-==-=-=-=-=-=-==-=-==-= */
+/*Ex1 - Repetição usando 'for': */
+/* for(c=1;c<=5;c+=1){
+    console.log(c)
+} */
+/* Ex2: */
+console.log('Vai começar...')
+for(c=1;c<=5;c+=1){
+    console.log(c)
+}
+console.log('Fim!')
