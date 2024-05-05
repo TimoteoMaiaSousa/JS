dd945d4a52b63c0615c1f78633d622116d66e71d
TimoteoMaiaSousa <156314160+TimoteoMaiaSousa@users.noreply.github.com>
2024-05-05 20:06:21 -0300

    ajustando...

--- exercicios/ex003/script.js
+++ exercicios/ex003/script.js
@@ -1,17 +1,25 @@
 function tabuada(){
     let num = document.getElementById('num')
     let tab = document.getElementById('seltab')
+    var av = document.getElementById('av')
     if(num.value.length == 0){
-        window.alert('Por favor, digite um número')
-    }else{
-        let n = Number(num.value)
-        let c = 1
+        av.innerHTML = 'Digite um número no espaço acima!'
+        av.style.color = 'red'
+        av.style.fontSize = '10pt'
+        av.style.textDecoration = 'underline'
         tab.innerHTML = ''
-        while(c <= 10){
-            let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
-            tab.appendChild(item)
-            c++
+    }else{
+            let n = Number(num.value)
+            let c = 1
+            av.innerHTML = `Tabuada de multiplicação do ${n}:`
+            av.style.fontSize = '10pt'
+            av.style.color = 'green'
+            tab.innerHTML = ''
+            while(c <= 10){
+                let item = document.createElement('option')
+                item.text = `${n} x ${c} = ${n*c}`
+                tab.appendChild(item)
+                c++
         }
     }
 }
