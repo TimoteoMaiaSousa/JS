3cd907e6969cadd61607fbb90cb22bcf8b53f4f2
TimoteoMaiaSousa <156314160+TimoteoMaiaSousa@users.noreply.github.com>
2024-05-05 11:17:59 -0300

    horario do dia

---
+++ exercicios/ex001/script.js
@@ -0,0 +1,23 @@
+ var user = prompt('Qual é o seu nome?')
+function carregar(){
+    var msg = document.getElementById('msg')
+    var hr = document.getElementById('hr')
+    var img = document.getElementById('img')
+    var data = new Date()
+    var hora = 14/* data.getHours() */
+    var min = data.getMinutes()
+    hr.innerHTML = `Agora são exatamente ${hora}:${min}`
+    if(hora >= 0 && hora <=12){
+        msg.innerHTML = `Bom dia, ${user}!`
+        img.src = 'manhã.jpg'
+        document.body.style.background = '#FBE882'
+    }else if(hora > 12 && hora <= 17){
+        msg.innerHTML = `Boa tarde, ${user}!`
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#67D4F0'
+    }else{
+        msg.innerHTML = `Boa noite, ${user}!`
+        img.src = 'noite.jpg'
+        document.body.style.background = '#1B1739'
+    }
+}
